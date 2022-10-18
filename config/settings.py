05646698bb1bda87f3e1cda0288978bbee3ef02b
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-10-18 18:03:10 +0900

    build: update config/settings.py

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    #
+    # サードパーティのアプリケーション
+    "rest_framework",
+    # 自作アプリケーション
+    "apiv1.apps.Apiv1Config",
+    "shop.apps.ShopConfig",
 ]
 
 MIDDLEWARE = [
@@ -55,7 +59,10 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            # テンプレートファイルの検索ディレクトリに BASE_DIR/templates を指定
+            os.path.join(BASE_DIR, "templates"),
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -100,13 +107,19 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# ログイン・ログアウト成功時のリダイレクト先の設定
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "rest_framework:login"
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+# 言語コードを修正
+LANGUAGE_CODE = "ja"
 
-TIME_ZONE = "UTC"
+# タイムゾーンを修正
+TIME_ZONE = "Asia/Tokyo"
 
 USE_I18N = True
 
@@ -120,7 +133,20 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
+# 静的ファイルの検索ディレクトリに BASE_DIR/static を指定
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# REST Framework
+REST_FRAMEWORK = {
+    # DRF の Cookie 認証の設定
+    "DEFAULT_AUTHENTICATION_CLASSES": {
+        "rest_framework.authentication.SessionAuthentication",
+    }
+}
